161e8424b3b9bd0dd62b81a69283ea7956e5c9f2
aakulgina <51416227+aakulgina@users.noreply.github.com>
2019-06-22 15:04:43 +0400

    Add files via upload

---
+++ telegram_bot/config.py
@@ -0,0 +1,5 @@
+greeting_text = 'Hello! I am a professional robo-artist. I can make a fascinating piece of art from any picture. If you do not believe me, just send me an image!'
+
+start_working_text = 'Nice choice! Now wait a bit, please, the artist starts working...'
+
+end_working_text = 'Your masterpiece is ready! :)'
