f065e8bca79d135ef749f526b7b8f8c21d57aa71
aakulgina <51416227+aakulgina@users.noreply.github.com>
2019-06-25 02:08:39 +0400

    Add one more phrase

--- telegram_bot/config.py
+++ telegram_bot/config.py
@@ -3,3 +3,5 @@ greeting_text = 'Hello! I am a professional robo-artist. I can make a fascinatin
 start_working_text = 'Nice choice! Now wait a bit, please, the artist starts working...'
 
 end_working_text = 'Your masterpiece is ready! :)'
+
+see_you_text = 'That is it! If you want one more piece of art - just send me another image. In case you forget something about me - text me /help'
