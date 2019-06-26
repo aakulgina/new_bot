24981115e126834dddfd11d33a2b4b15a38f9d91
aakulgina <51416227+aakulgina@users.noreply.github.com>
2019-06-26 22:41:06 +0400

    Add docstring, check code style

--- telegram_bot/config.py
+++ telegram_bot/config.py
@@ -1,7 +1,17 @@
-greeting_text = 'Hello! I am a professional robo-artist. I can make a fascinating piece of art from any picture. If you do not believe me, just send me an image!'
+"""
+Модуль содержит тексты сообщений, которые бот отправляет пользователю.
 
-start_working_text = 'Nice choice! Now wait a bit, please, the artist starts working...'
+"""
+
+greeting_text = ('Hello! I am a professional robo-artist.'
+    'I can make a fascinating piece of art from any picture.'
+    'If you do not believe me, just send me an image!')
+
+start_working_text = ('Nice choice! Now wait a bit, please,'
+    'the artist starts working...')
 
 end_working_text = 'Your masterpiece is ready! :)'
 
-see_you_text = 'That is it! If you want one more piece of art - just send me another image. In case you forget something about me - text me /help'
+see_you_text = ('That is it! If you want one more'
+    'piece of art - just send me another image.'
+    'In case you forget something about me - text me /help')
