1237dfab2d341709a14e7a40e00ffc1b737ed9c3
aakulgina <51416227+aakulgina@users.noreply.github.com>
2019-06-26 22:42:43 +0400

    Add docstring, check code style

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -1,27 +1,52 @@
-from model import StyleTransferModel
-from config import greeting_text, start_working_text, end_working_text, see_you_text
-from telegram_token import token
-from telegram.ext import CommandHandler
+"""
+Данный модуль содержит основной код бота.
+
+Бот устроен так, чтобы каждое его действие
+по отношению к пользователю отображалось в логгинге.
+Это сделано для того, чтобы было проще отследить,
+на каком конкретно этапе возникла ошибка.
+
+"""
+
+from io import BytesIO
 import numpy as np
 from PIL import Image
-from io import BytesIO
+from telegram.ext import CommandHandler
+from config import end_working_text, see_you_text
+from config import greeting_text, start_working_text
+from model import StyleTransferModel
+from telegram_token import token
+
 
 model = StyleTransferModel()
 content_image_file = {}
 
+
 def greet_n_explain(bot, update):
+    """
+    Отправляет пользователю приветственное сообщение.
+
+    Помимо команды /start, на эту функцию
+    ссылается также команда /help.
+
+    """
     chat_id = update.message.chat_id
     bot.send_message(chat_id, text=greeting_text)
-    print("Greeted {} and explained them the working principle".format(chat_id))
+    print("Greeted {} and explained the working principle".format(chat_id))
+
 
 def send_prediction_on_photo(bot, update):
-    # Нам нужно получить одну картинку, которая станет content image.
+    """
+    Достает из чата фото, обрабатывает его и присылает результат.
+
+    """
+    # Получаем одну картинку, которая станет картинкой контента.
     chat_id = update.message.chat_id
     print("Got image from {}".format(chat_id))
     bot.send_message(chat_id, text=start_working_text)
     print("Notified {} about start of the working process".format(chat_id))
 
-    # получаем информацию о картинке
+    # Получаем информацию о картинке.
     image_info = update.message.photo[-1]
     image_file = bot.get_file(image_info)
     content_image_file[chat_id] = image_file
@@ -32,7 +57,7 @@ def send_prediction_on_photo(bot, update):
 
     output = model.transfer_style(content_image_stream)
 
-    # теперь отправим назад фото
+    # Теперь отправим назад фото.
     output_stream = BytesIO()
     output.save(output_stream, format='PNG')
     output_stream.seek(0)
@@ -48,16 +73,15 @@ if __name__ == '__main__':
     from telegram.ext import Updater, MessageHandler, Filters
     import logging
 
-    # Включим самый базовый логгинг, чтобы видеть сообщения об ошибках
+    # Включим самый базовый логгинг, чтобы видеть сообщения об ошибках.
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         level=logging.INFO)
-    # используем прокси
-    updater = Updater(token=token, request_kwargs={'proxy_url': 'socks4://89.169.8.89:4145'})
 
-    # В реализации большого бота скорее всего будет удобнее использовать Conversation Handler
-    # вместо назначения handler'ов таким способом
-    updater.dispatcher.add_handler(CommandHandler('start',greet_n_explain))
-    updater.dispatcher.add_handler(CommandHandler('help',greet_n_explain))
+    updater = Updater(token=token,
+        request_kwargs={'proxy_url': 'socks4://89.169.8.89:4145'})
+
+    updater.dispatcher.add_handler(CommandHandler('start', greet_n_explain))
+    updater.dispatcher.add_handler(CommandHandler('help', greet_n_explain))
     updater.dispatcher.add_handler(MessageHandler(Filters.photo, send_prediction_on_photo))
     updater.start_polling()
