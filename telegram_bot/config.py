2f6ec059b68add94c573c8854d0185742d23c6b7
aakulgina <51416227+aakulgina@users.noreply.github.com>
2019-06-27 17:06:30 +0400

    Update config.py

--- telegram_bot/config.py
+++ telegram_bot/config.py
@@ -3,15 +3,15 @@
 
 """
 
-greeting_text = ('Hello! I am a professional robo-artist.'
-    'I can make a fascinating piece of art from any picture.'
+greeting_text = ('Hello! I am a professional robo-artist. '
+    'I can make a fascinating piece of art from any picture. '
     'If you do not believe me, just send me an image!')
 
-start_working_text = ('Nice choice! Now wait a bit, please,'
+start_working_text = ('Nice choice! Now wait a bit, please, '
     'the artist starts working...')
 
 end_working_text = 'Your masterpiece is ready! :)'
 
-see_you_text = ('That is it! If you want one more'
-    'piece of art - just send me another image.'
+see_you_text = ('That is it! If you want one more '
+    'piece of art - just send me another image. '
     'In case you forget something about me - text me /help')
